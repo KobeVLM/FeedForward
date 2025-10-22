f4a88eb1013607179e690e819a103de9efe92b42
Kyube <kobemarikit70@gmail.com>
2025-10-23 00:40:49 +0800

    feat: implement Department CRUD operations and update application properties

---
+++ backend/feedforward/src/main/java/com/bibit/feedforward/feedforward/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.bibit.feedforward.feedforward.repository;
+
+import com.bibit.feedforward.feedforward.entity.DepartmentEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Long> {
+
+}
